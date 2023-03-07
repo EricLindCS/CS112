28d8ae0e138f3077bee1c8075a7ef00dfefdf608
Eric Lind <107385095+EricLindCS@users.noreply.github.com>
2023-03-06 20:22:25 -0800

    external JS and reorganization

---
+++ Main Website/JavaScript/aboutJS.js
@@ -0,0 +1,7 @@
+function confirmSubmit() {
+    return confirm("Are you sure you want to submit the form?");
+  }
+  function showConfirmation() {
+       alert("Thank you for submitting the form!");
+       return true;
+}
---
+++ Main Website/JavaScript/homeJS.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){  
+    $("#hp").hover(function(){  
+      $(this).attr('src','../images/mocha1.jpg');  
+      }, function(){  
+      $(this).attr('src','../images/mocha.jpg');  
+    }); 
+    $("#hp2").hover(function(){  
+      $(this).attr('src','../images/lilie1.jpg');  
+      }, function(){  
+      $(this).attr('src','../images/lilie.jpg');  
+    });  
+  });  
