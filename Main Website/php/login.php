3052f9fe59c1e3d1491d5504e36c1230e5cb3647
Eric Lind <107385095+EricLindCS@users.noreply.github.com>
2023-03-27 19:13:09 -0700

    Fix SQL

--- Main Website/php/login.php
+++ Main Website/php/login.php
@@ -1,34 +1,54 @@
 <?php
-// Connect to database
-$host="127.0.0.1";
-$port=3306;
-$socket="";
-$user="root";
-$password="lamnoda3";
-$dbname="cs116";
-
-$conn = new mysqli($host, $user, $password, $dbname, $port, $socket)
-	or die ('Could not connect to the database server' . mysqli_connect_error());
-
-//$con->close();
-
-// Check connection
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
+// connect to database
+$host = 'localhost';
+$user = 'username';
+$password = 'password';
+$dbname = 'logininfo';
+$dsn = "mysql:host=$host;dbname=$dbname";
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+];
+try {
+    $pdo = new PDO($dsn, $user, $password, $options);
+} catch(PDOException $e) {
+    echo $e->getMessage();
 }
 
-// Get form data
-$name = mysqli_real_escape_string($conn, $_POST['username']);
-$email = mysqli_real_escape_string($conn, $_POST['password']);
+// check if form was submitted
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // get form data
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-// Insert data into table
-$sql = "INSERT INTO contactinfo (`username`, `password`) VALUES ('$name', '$email')";
-if (mysqli_query($conn, $sql)) {
-  echo "New record created successfully";
-} else {
-  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    // query database for user credentials
+    $sql = "SELECT * FROM users WHERE username = :username";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':username', $username);
+    $stmt->execute();
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    // check if user exists and password is correct
+    if ($user && password_verify($password, $user['password'])) {
+        // login successful
+        echo "Login successful!";
+    } else {
+        // login unsuccessful
+        // increment login attempts
+        session_start();
+        if (!isset($_SESSION['attempts'])) {
+            $_SESSION['attempts'] = 1;
+        } else {
+            $_SESSION['attempts']++;
+        }
+
+        // check if user has been locked out
+        if ($_SESSION['attempts'] >= 3) {
+            // lock user out
+            echo "You have been locked out!";
+        } else {
+            // show error message
+            echo "Incorrect username or password. Please try again.";
+        }
+    }
 }
-
-// Close connection
-mysqli_close($conn);
 ?>
